53a0c277ed5955baac5f8e54db11ce70b97dbcb5
mani <mani@guerillasoftware.net>
2013-11-25 14:39:29 +0000

    .

--- app/app/market/static/js/market.js
+++ app/app/market/static/js/market.js
@@ -2,16 +2,24 @@
 
     var MarketRoute = Backbone.Router.extend({
         routes:{
+            "": "page",
             "p:page": "page"
         },
+        
         page: function(page){
-            $('#marketitems').empty();
-            this.market.setItems(parseInt(page)-1);
+            if(page){
+                $('#marketitems').empty();
+                this.market.setItems(parseInt(page)-1);
+            }else{
+                this.market.setItems(0);
+            }
         },
+        
         initialize: function(market){
-            this.market=market;
+            this.market=market;            
         }
     });
+    
     
     var MarketView = Backbone.View.extend({        
         el: '#market',        
@@ -21,121 +29,120 @@
             'click .tagbutton': 'filter'
         },
         
-
-    search: function(){
-        window.getcsrf(function(csrf){                
-            var data= {
-                q:$('#q').val(),
-                csrfmiddlewaretoken:csrf.csrfmiddlewaretoken,
-            };
-            window.location = 'search?'+$.param(data);
-        });            
-    },
-
-    showItem: function(ev){
-        var id = ev.currentTarget.getAttribute('item_id');
-        window.location = window.app_urls.viewitem+id;
-    },
-
-    getItems: function(from,to,filts){           
-        return $.ajax({
-            url:window.app_urls.getmarketitemfromto.replace('0',from)+to,
-            dataType: 'json',
-            contentType:"application/json; charset=utf-8",
-            data: filts,
-            traditional: true
-        });
-    },
+        search: function(){
+            window.getcsrf(function(csrf){                
+                var data= {
+                    q:$('#q').val(),
+                    csrfmiddlewaretoken:csrf.csrfmiddlewaretoken,
+                };
+                window.location = 'search?'+$.param(data);
+            });            
+        },
     
-    getItemsCount: function(filts){           
-        return $.ajax({               
-            url:window.app_urls.getmarketcount,
-            dataType: 'json',
-            contentType:"application/json; charset=utf-8",
-            data: filts,
-            traditional: true
-        });
-    },
+        showItem: function(ev){
+            var id = ev.currentTarget.getAttribute('item_id');
+            window.location = window.app_urls.viewitem+id;
+        },
     
-    
-    filter: function(ev){
-        var that=this;
-        a=$(ev.currentTarget.parentElement.parentElement).attr("item_title");
-        ar = this.filters[a];
-        inv = invert(window[a]);
-        if (inv.hasOwnProperty(ev.currentTarget.textContent)){
-            ind = inv[ev.currentTarget.textContent];
-            filtind = ar.indexOf(parseInt(ind));
-            if(filtind<0){
-                that.filters[a].push(parseInt(ind));
-                $(ev.currentTarget).addClass('btn-success');
-            }else{
-                that.filters[a].splice(filtind,1);
-                $(ev.currentTarget).removeClass('btn-success');
-            }
-        }
-        $('#marketitems').empty();
-        this.setItems(0);
-        
-    },
-    
-    getfilter: function(){
-        $.noop();
-    },
-    
-    initFilters:function(items){       
-        var that = this;        
-        _.each(window[items],function(item,key){            
-            if (that.filters[items].indexOf(parseInt(key))>-1){
-                 $.noop();
-            }else{
-                $('.row.btn-group-sm.'+items).append(that.tagtemp({filtertag:item, active:' '}));
-            }
-        }); 
-    },
-    
-    setFilters: function(){
-        var that = this;                
-        for(item_ind in that.filters){
-            var item = that.filters[item_ind];
-            for(ind in item){
-                var i = item[ind];
-                $('.row.btn-group-sm.'+item_ind).append(that.tagtemp({filtertag:window[item_ind][i], active:'btn-success'}));
-            }        
-        }         
-    },
-    
-    setItems: function(page){
-        var that = this;
-        var dfrd = this.getItems(0+(10*page),10+(10*page),this.filters);
-        dfrd.done(function(data){
-            $.each(data, function(item){
-                data[item].fields.pk = data[item].pk;
-                var item_html = that.item_tmp(data[item].fields);
-                $('#marketitems').append(item_html);
+        getItems: function(from,to,filts){           
+            return $.ajax({
+                url:window.app_urls.getmarketitemfromto.replace('0',from)+to,
+                dataType: 'json',
+                contentType:"application/json; charset=utf-8",
+                data: filts,
+                traditional: true
             });
-        });
-       
-    },
-
-    initialize : function(filters){
-        var that = this;     
-        this.tagtemp = _.template($('#filter-tag').html());
-        this.item_tmp = _.template($('#item_template').html());
-        that.filters = filters;
-        this.setFilters(filters);
-        for(item_ind in that.filters){
-            that.initFilters(item_ind);
-        }
-        this.setItems(0);
-        cdfrd = this.getItemsCount(this.filters);
-        cdfrd.done(function(data){
-            var pages = Math.ceil(data.count/10);
-            for(i=0;i<pages;i++){
-                $(".marketitems.pagination").append("<li><a class='itempage' page='"+i+"' href='#p"+(i+1)+"'>"+(i+1)+"</a></li>");
+        },
+        
+        getItemsCount: function(filts){           
+            return $.ajax({               
+                url:window.app_urls.getmarketcount,
+                dataType: 'json',
+                contentType:"application/json; charset=utf-8",
+                data: filts,
+                traditional: true
+            });
+        },
+        
+        
+        filter: function(ev){
+            var that=this;
+            a=$(ev.currentTarget.parentElement.parentElement).attr("item_title");
+            ar = this.filters[a];
+            inv = invert(window[a]);
+            if (inv.hasOwnProperty(ev.currentTarget.textContent)){
+                ind = inv[ev.currentTarget.textContent];
+                filtind = ar.indexOf(parseInt(ind));
+                if(filtind<0){
+                    that.filters[a].push(parseInt(ind));
+                    $(ev.currentTarget).addClass('btn-success');
+                }else{
+                    that.filters[a].splice(filtind,1);
+                    $(ev.currentTarget).removeClass('btn-success');
+                }
             }
-        });
-    },
+            $('#marketitems').empty();
+            this.setItems(0);
+            
+        },
+        
+        getfilter: function(){
+            $.noop();
+        },
+        
+        initFilters:function(items){       
+            var that = this;        
+            _.each(window[items],function(item,key){            
+                if (that.filters[items].indexOf(parseInt(key))>-1){
+                     $.noop();
+                }else{
+                    $('.row.btn-group-sm.'+items).append(that.tagtemp({filtertag:item, active:' '}));
+                }
+            }); 
+        },
+        
+        setFilters: function(){
+            var that = this;                
+            for(item_ind in that.filters){
+                var item = that.filters[item_ind];
+                for(ind in item){
+                    var i = item[ind];
+                    $('.row.btn-group-sm.'+item_ind).append(that.tagtemp({filtertag:window[item_ind][i], active:'btn-success'}));
+                }        
+            }         
+        },
+        
+        setItems: function(page){
+            var that = this;
+            var dfrd = this.getItems(0+(10*page),10+(10*page),this.filters);
+            dfrd.done(function(data){
+                $.each(data, function(item){
+                    data[item].fields.pk = data[item].pk;
+                    var item_html = that.item_tmp(data[item].fields);
+                    $('#marketitems').append(item_html);
+                });
+            });
+           
+        },
+    
+        initialize : function(filters){
+            var that = this;     
+            this.tagtemp = _.template($('#filter-tag').html());
+            this.item_tmp = _.template($('#item_template').html());
+            that.filters = filters;
+            this.setFilters(filters);
+            for(item_ind in that.filters){
+                that.initFilters(item_ind);
+            }       
+            cdfrd = this.getItemsCount(this.filters);
+            cdfrd.done(function(data){
+                var pages = Math.ceil(data.count/10);
+                for(i=0;i<pages;i++){
+                    $(".marketitems.pagination").append("<li><a class='itempage' page='"+i+"' href='#p"+(i+1)+"'>"+(i+1)+"</a></li>");
+                }
+            });
+            return this;
+        },
     });
 
     window.market = window.market || {};
@@ -143,6 +150,6 @@
         window.default_filters = filters;
         var market = new MarketView(filters);
         var market_route = new MarketRoute(market);
-        Backbone.history.start();
+        Backbone.history.start();        
     };
 })();
