c5c9476a592467b9141a6c1cc79aac4f3ff5ee64
Aidan Hamade <aidan.hamade@gmail.com>
2013-12-04 12:27:15 +0000

    mess around with messages

--- app/app/market/middleware.py
+++ app/app/market/middleware.py
@@ -2,7 +2,7 @@ from django.shortcuts import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
 class NoUserProfile(object):
-    def process_request(self,request):
+    def process_request(self, request):
         if not request.user.is_anonymous() and not hasattr(request.user,'userprofile') and (
             request.path != reverse('getting_started') and
             request.path != reverse('account_logout')
