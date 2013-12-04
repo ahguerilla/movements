d6d8d160bad48398b435341fa246e50819e7ee46
mani <mani@guerillasoftware.net>
2013-12-04 11:45:45 +0000

    some clean up

--- app/app/market/static/js/base.js
+++ app/app/market/static/js/base.js
@@ -1,4 +1,5 @@
 (function(){
+
     setInterval(function(){
         $.getJSON(window.ahr.app_urls.getmessagecount,function(data){
             $('.message-counter').each(function(tmp,item){
@@ -12,113 +13,220 @@
         });
     }
     ,30000);    
+   
+    window.ahr= window.ahr || {};
+    window.ahr.BaseView = Backbone.View.extend({	
+	tagdict: {},
+	events:{
+	    'click .sendprivatemessageuser': 'showpMessage',
+	    'click .sendpm': 'sendpm',
+	    'click .cancelpm': 'cancelpm',
+	    'click .btn.rate': 'showrate',
+	    'click .sendrate': 'setrate',
+	    'click .cancelrate': 'resetrate'
+	},
+	
+	invert: function (obj) {
+	    var new_obj = {};
+	    for (var prop in obj) {
+	      if(obj.hasOwnProperty(prop)) {
+		new_obj[obj[prop]] = prop;
+	      }
+	    }
+	    return new_obj;
+	},
+    
+	generateTypeAhead:function(title,  item){
+	    if (this.typeAheadTag  == undefined){
+		this.typeAheadTag = _.template($('#typeahead_template').html());
+	    }
+	    return this.typeAheadTag({'title':title,'item':item});
+	},
+    
+	setDateTimePicker: function(item,preval){	
+	    if(preval){														
+		$('#'+item.jsonfield).val(preval.slice(8,10)+'/'+
+		    preval.slice(5,7)+'/'+
+		    preval.slice(0,4)+' '+
+		    preval.slice(11,13)+':'+
+		    preval.slice(14,16)
+		);
+	    }
+	},
+	
+	makeTypeAhead: function(jsonfield, aurl,func){
+	    var that = this;
+	    var typeaheadval = [],values=[];
+	    $.getJSON(aurl,function(data){
+		for (var i = 0; i < data.length; i++){
+		    typeaheadval.push({
+			tokens: [data[i].fields[jsonfield],],
+			value: data[i].fields[jsonfield]
+		    });
+		    values.push(data[i].fields[jsonfield]);
+		    that.tagdict[data[i].fields[jsonfield]]=data[i].pk;
+		}
+		$('#'+jsonfield).typeahead({
+		    limit: 5,
+		    local: typeaheadval
+		    }).on('typeahead:selected', function (e, d) {
+			$('#'+jsonfield).tagsManager("pushTag", d.value);
+		});
+	    func(values);
+	    });
+	},
+    
+	makeTagWidget:function (jsonfield, aurl,prefilled){
+	    var that = this;
+	    this.makeTypeAhead(jsonfield, aurl,function(data){
+		var pref=[],values = data;
+		if (prefilled){
+		    inv = that.invert(that.tagdict);
+		    _.each(prefilled,function(id){
+			pref.push(inv[id]);
+		    });
+		}
+    
+	    $('#'+jsonfield).tagsManager({
+		tagsContainer: '#'+jsonfield+'Tags',
+		deleteTagsOnBackspace: false,
+		blinkBGColor_1: '#FFFF9C',
+		blinkBGColor_2: '#CDE69C',
+		hiddenTagListName: 'hidden-'+jsonfield,
+		prefilled: pref,
+		validator:function(tag){
+		    if (values.indexOf(tag)!=-1){
+			return true;
+		    }else{
+			return false;
+		    }
+		},
+	    });
+	  });
+	},
+    
+	getTagIds: function(name){
+	    var that = this;
+	    var val_ids=[],
+		val_names = $('input[name="hidden-'+name+'"]').val().split(',');
+	    _.each(val_names,function(val){
+		val_ids.push(that.tagdict[val]);
+	    });
+	    return val_ids;
+	},
+    
+	genTagWidget: function (item, preval){
+	    var that = this;
+	    $('#'+item.jsonfield+'_place').html(that.generateTypeAhead(item.title, item.jsonfield));
+	    if (item.jsonfield == 'skills'){
+		this.makeTagWidget(item.jsonfield,window.ahr.app_urls.getskills, preval);
+	    }else if (item.jsonfield == 'issues'){
+		this.makeTagWidget(item.jsonfield,window.ahr.app_urls.getissues, preval);
+	    }else{
+		this.makeTagWidget(item.jsonfield,window.ahr.app_urls.getcountries, preval);
+	    }
+    
+	},
+    
+	afterDateTimePicker: function(item){
+	    $('#'+item.type+'-'+item.jsonfield).datetimepicker({format:'D/M/YYYY HH:mm'});
+	},
+    
+	alert: function(message,selector){
+	    $(selector).empty();
+	    $(selector).prepend('<div class="alert alert-warning alert-dismissable">'+
+	    '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'+
+	    message+'</div>');
+	},
+    
+	info: function (message,selector){
+	    $(selector).empty();
+	    $(selector).prepend('<div class="alert alert-success alert-dismissable">'+
+		'<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'+
+		message+'</div>');
+	},
+    
+	sendpm:function(ev){
+	    var that = this;	
+	    if( $('#msgsub').val()!= '' &&  $('#newmessage').val() != ''){
+		$('#messagedialog').modal('hide');
+		window.getcsrf(function(csrf){
+		    var dfrd = $.ajax({
+			url:window.ahr.app_urls.sendmessage+$('#usernameh').text(),
+			type: 'POST',
+			dataType: 'json',
+			data: {
+			    csrfmiddlewaretoken :csrf.csrfmiddlewaretoken,
+			    subject: $('#msgsub').val(),
+			    message: $('#newmessage').val()
+			}
+		    });
+		    dfrd.done(function(){	       
+			$('#market').prepend('<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Your message was sent successfuly.</div>');
+		    });
+		});    
+	    }else{
+		this.alert('Please provide a subject and message.','#pmerror');
+	    }    
+	},
+    
+    
+	cancelpm:function(ev){
+	},
+    
+	showpMessage: function(ev){
+	    var username = ev.currentTarget.getAttribute('username');            
+	    $('#usernameh').text(username);
+	    $('#messagedialog').modal('show');
+	}, 
+    
+	showrate: function(ev){
+	    var username = ev.currentTarget.getAttribute('username');
+	    var image_src = ev.currentTarget.getAttribute('image_src');
+	    var score = ev.currentTarget.getAttribute('score');
+	    var ratecount = ev.currentTarget.getAttribute('ratecount');
+	    $('#ratetitle').text(username);
+	    $('#username').text(username);
+	    $('#ratecount').text(ratecount);
+	    $('#numstars').html('<div class="stars'+parseInt(Math.round(score))+'"></div>');
+	    $('#profileimage').attr('src',image_src);
+	    $('#ratedialog').modal('show');
+	},
+    
+	setrate: function(ev){
+	    var that = this;
+	    if($('input[name="stars"]:checked').val() == undefined){
+		this.alert('You have to select a rateing.','#rateerror');
+		return;
+	    }
+	    window.getcsrf(function(csrf){
+		if ($('.btn.rate').attr('rateing')=='user'){
+		    this.rateurl = window.ahr.app_urls.setuserrate+$('#username').text();
+		}else{
+		}
+		var dfrd = $.ajax({
+		    url: this.rateurl,
+		    type: 'POST',
+		    dataType: 'json',
+		    data: {
+			score:$('input[name="stars"]:checked').val(),
+			csrfmiddlewaretoken :csrf.csrfmiddlewaretoken,
+			}
+		});
+		dfrd.done(function(data){
+		    $('.btn.rate').attr('ratecount',data.ratecount);
+		    $('.btn.rate').attr('score',data.score);
+		    $('#ratedialog').modal('hide');
+		    that.resetrate();
+		});
+		dfrd.fail(function(data){
+		    that.alert('Rating failed.','#rateerror');
+		});
+	    });
+	},
+    
+	resetrate: function(){
+	    $('input[name="stars"]:checked').prop('checked',false);
+	},
+    });
 })();
-
-window.ahr= window.ahr || {};
-window.ahr.BaseView = Backbone.View.extend({
-    events:{
-        'click .sendprivatemessageuser': 'showpMessage',
-        'click .sendpm': 'sendpm',
-        'click .cancelpm': 'cancelpm',
-        'click .btn.rate': 'showrate',
-        'click .sendrate': 'setrate',
-        'click .cancelrate': 'resetrate'
-    },
-
-    alert: function(message,selector){
-        $(selector).empty();
-        $(selector).prepend('<div class="alert alert-warning alert-dismissable">'+
-        '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'+
-        message+'</div>');
-    },
-
-    info: function (message,selector){
-        $(selector).empty();
-        $(selector).prepend('<div class="alert alert-success alert-dismissable">'+
-            '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'+
-            message+'</div>');
-    },
-
-    sendpm:function(ev){
-        var that = this;	
-        if( $('#msgsub').val()!= '' &&  $('#newmessage').val() != ''){
-            $('#messagedialog').modal('hide');
-            window.getcsrf(function(csrf){
-                var dfrd = $.ajax({
-                    url:window.ahr.app_urls.sendmessage+$('#usernameh').text(),
-                    type: 'POST',
-                    dataType: 'json',
-                    data: {
-                        csrfmiddlewaretoken :csrf.csrfmiddlewaretoken,
-                        subject: $('#msgsub').val(),
-                        message: $('#newmessage').val()
-                    }
-                });
-                dfrd.done(function(){	       
-                    $('#market').prepend('<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Your message was sent successfuly.</div>');
-                });
-            });    
-        }else{
-            this.alert('Please provide a subject and message.','#pmerror');
-        }    
-    },
-
-
-    cancelpm:function(ev){
-    },
-
-    showpMessage: function(ev){
-        var username = ev.currentTarget.getAttribute('username');            
-        $('#usernameh').text(username);
-        $('#messagedialog').modal('show');
-    }, 
-
-    showrate: function(ev){
-        var username = ev.currentTarget.getAttribute('username');
-        var image_src = ev.currentTarget.getAttribute('image_src');
-        var score = ev.currentTarget.getAttribute('score');
-        var ratecount = ev.currentTarget.getAttribute('ratecount');
-        $('#ratetitle').text(username);
-        $('#username').text(username);
-        $('#ratecount').text(ratecount);
-        $('#numstars').html('<div class="stars'+parseInt(Math.round(score))+'"></div>');
-        $('#profileimage').attr('src',image_src);
-        $('#ratedialog').modal('show');
-    },
-
-    setrate: function(ev){
-        var that = this;
-        if($('input[name="stars"]:checked').val() == undefined){
-            this.alert('You have to select a rateing.','#rateerror');
-            return;
-        }
-        window.getcsrf(function(csrf){
-            if ($('.btn.rate').attr('rateing')=='user'){
-                this.rateurl = window.ahr.app_urls.setuserrate+$('#username').text();
-            }else{
-            }
-            var dfrd = $.ajax({
-                url: this.rateurl,
-                type: 'POST',
-                dataType: 'json',
-                data: {
-                    score:$('input[name="stars"]:checked').val(),
-                    csrfmiddlewaretoken :csrf.csrfmiddlewaretoken,
-                    }
-            });
-            dfrd.done(function(data){
-                $('.btn.rate').attr('ratecount',data.ratecount);
-                $('.btn.rate').attr('score',data.score);
-                $('#ratedialog').modal('hide');
-                that.resetrate();
-            });
-            dfrd.fail(function(data){
-                that.alert('Rating failed.','#rateerror');
-            });
-        });
-    },
-
-    resetrate: function(){
-        $('input[name="stars"]:checked').prop('checked',false);
-    },
-});
--- app/app/market/static/js/item_form.js
+++ app/app/market/static/js/item_form.js
@@ -10,136 +10,142 @@ function progressHandlingFunction(e){
         el: '#itemform',
         item_type: '',
         item:'',
+        form_title:{
+            'offer': 'OFFER A SERVICE',
+            'resource': 'SHARE A RESOURCE',
+            'request': 'REQUEST A SERVICE'
+        },
+        
+        setWidgetArr:function(){    
+            var that = this;
+            this.widget_arrs = {
+                'offer':[
+                    {type: 'typeahead', title:'I can help to advance freedom of:', jsonfield:'issues', customGen: that.genTagWidget, customGet: that.getTagIds },
+                    {type: 'typeahead', title:'Please select the countries where you can help',  jsonfield:'countries', customGen: that.genTagWidget, customGet: that.getTagIds},
+                    {type: 'typeahead', title:'Skills',  jsonfield:'skills', customGen: that.genTagWidget, customGet:that.getTagIds},
+                    {type: 'datetimepicker', title:'Expiry date', jsonfield:'exp_date', placeholder:'', customSet:that.setDateTimePicker, afterGen:that.afterDateTimePicker },
+                    {type: 'input', title:'Title of post', jsonfield:'title', placeholder:''},
+                    {type: 'textarea', title:'', jsonfield:'details', placeholder:'Please give details of what you can help with?'}
+                ],
+                'request':[
+                    {type: 'typeahead', title:'I need help to advance freedom of:', jsonfield:'issues', customGen: that.genTagWidget, customGet: that.getTagIds},
+                    {type: 'typeahead', title:'Please select the countries where you need help',  jsonfield:'countries', customGen: that.genTagWidget, customGet:that.getTagIds},
+                    {type: 'datetimepicker', title:'Expiry date', jsonfield:'exp_date', placeholder:'', customSet:that.setDateTimePicker, afterGen:that.afterDateTimePicker },
+                    {type: 'input', title:'Title of post', jsonfield:'title', placeholder:''},
+                    {type: 'textarea', title:'', jsonfield:'details', placeholder:'Please give details of what you can help with?'}
+                ],
+                'resource':[
+                    {type: 'typeahead', title:'Issues', jsonfield:'issues', customGen: that.genTagWidget, customGet:that.getTagIds },
+                    {type: 'typeahead', title:'Country',  jsonfield:'countries', customGen: that.genTagWidget, customGet:that.getTagIds},
+                    {type: 'typeahead', title:'Skills',  jsonfield:'skills', customGen: that.genTagWidget, customGet:that.getTagIds},
+                    {type: 'datetimepicker', title:'Expiry date', jsonfield:'exp_date', placeholder:'', customSet:that.setDateTimePicker, afterGen:that.afterDateTimePicker },
+                    {type: 'input', title:'Title of post', jsonfield:'title', placeholder:''},
+                    {type: 'input', title:'URL link', jsonfield:'url', placeholder:''},
+                    {type: 'textarea', title:'', jsonfield:'details', placeholder:'Add a description'}
+                ]
+            };
+        },
     
-    form_title:{
-        'offer': 'OFFER A SERVICE',
-        'resource': 'SHARE A RESOURCE',
-        'request': 'REQUEST A SERVICE'
-    },
-
-    widget_arrs: {
-        'offer':[
-            {type: 'typeahead', title:'I can help to advance freedom of:', jsonfield:'issues', customGen: genTagWidget, customGet:getTagIds },
-            {type: 'typeahead', title:'Please select the countries where you can help',  jsonfield:'countries', customGen: genTagWidget, customGet:getTagIds},
-            {type: 'typeahead', title:'Skills',  jsonfield:'skills', customGen: genTagWidget, customGet:getTagIds},
-            {type: 'datetimepicker', title:'Expiry date', jsonfield:'exp_date', placeholder:'',	customSet:setDateTimePicker, afterGen:afterDateTimePicker },
-            {type: 'input', title:'Title of post', jsonfield:'title', placeholder:''},
-            {type: 'textarea', title:'', jsonfield:'details', placeholder:'Please give details of what you can help with?'}
-        ],
-        'request':[
-            {type: 'typeahead', title:'I need help to advance freedom of:', jsonfield:'issues', customGen: genTagWidget, customGet:getTagIds},
-            {type: 'typeahead', title:'Please select the countries where you need help',  jsonfield:'countries', customGen: genTagWidget, customGet:getTagIds},
-            {type: 'datetimepicker', title:'Expiry date', jsonfield:'exp_date', placeholder:'',	customSet:setDateTimePicker, afterGen:afterDateTimePicker },
-            {type: 'input', title:'Title of post', jsonfield:'title', placeholder:''},
-            {type: 'textarea', title:'', jsonfield:'details', placeholder:'Please give details of what you can help with?'}
-        ],
-        'resource':[
-            {type: 'typeahead', title:'Issues', jsonfield:'issues', customGen: genTagWidget, customGet:getTagIds },
-            {type: 'typeahead', title:'Country',  jsonfield:'countries', customGen: genTagWidget, customGet:getTagIds},
-            {type: 'typeahead', title:'Skills',  jsonfield:'skills', customGen: genTagWidget, customGet:getTagIds},
-            {type: 'datetimepicker', title:'Expiry date', jsonfield:'exp_date', placeholder:'',	customSet:setDateTimePicker, afterGen:afterDateTimePicker },
-            {type: 'input', title:'Title of post', jsonfield:'title', placeholder:''},
-            {type: 'input', title:'URL link', jsonfield:'url', placeholder:''},
-            {type: 'textarea', title:'', jsonfield:'details', placeholder:'Add a description'}
-        ]
-    },
-
-    getFormData: function(){
-        var retdict={};
-        _.each(this.widget_arrs[this.item_type],function(item){
-            if(item.customGet){
-                retdict[item.jsonfield] = item.customGet(item.jsonfield);
-            }else{
-
-                retdict[item.jsonfield] = $('#'+item.jsonfield).val();
-            }
-        });
-        retdict.csrfmiddlewaretoken=$('input[name="csrfmiddlewaretoken"]').val();
-        return retdict;
-    },
-
-    getItem: function(item,func){
-        this.item=$.getJSON(window.ahr.app_urls.getmarketitem + item.id, func);
-    },
-
-
-    setForm: function(data,obj_type){
-        var that=this;
-        window.tempfields = data[0].fields;
-
-        _.each(this.widget_arrs[obj_type],function(item){
-            that.makeWidget(item,window.tempfields[item.jsonfield]);
-        });
-    },
-
-    makeWidget: function(item,preval){
-        var that = this;
-        if(item.customGen){
-            item.customGen(item,preval);
-        }else{
-            var tmpl = _.template($('#'+item.type+'_template').html()),
-            widget = tmpl({'title':item.title,
-                'jsonfield': item.jsonfield,
-                'placeholder': item.placeholder
-            });
-            $('#'+item.jsonfield+'_place').html(widget);
-            if(preval){
-                if(item.customSet){
-                    item.customSet(item,preval);
+        getFormData: function(){
+            var that = this;
+            var retdict={};
+            _.each(this.widget_arrs[this.item_type],function(item){
+                if(item.customGet){
+                    var func = _.bind(item.customGet,that);
+                    retdict[item.jsonfield] = func(item.jsonfield);
                 }else{
-                    $('#'+item.jsonfield).val(preval);
+    
+                    retdict[item.jsonfield] = $('#'+item.jsonfield).val();
+                }
+            });
+            retdict.csrfmiddlewaretoken=$('input[name="csrfmiddlewaretoken"]').val();
+            return retdict;
+        },
+    
+        getItem: function(item,func){
+            this.item=$.getJSON(window.ahr.app_urls.getmarketitem + item.id, func);
+        },
+    
+    
+        setForm: function(data,obj_type){
+            var that=this;
+            window.tempfields = data[0].fields;
+    
+            _.each(this.widget_arrs[obj_type],function(item){
+                that.makeWidget(item,window.tempfields[item.jsonfield]);
+            });
+        },
+    
+        makeWidget: function(item,preval){
+            var that = this;
+            if(item.customGen){
+                var func = _.bind(item.customGen,this);
+                func(item,preval);
+            }else{
+                var tmpl = _.template($('#'+item.type+'_template').html());
+                var widget = tmpl({'title':item.title,
+                    'jsonfield': item.jsonfield,
+                    'placeholder': item.placeholder
+                });
+                $('#'+item.jsonfield+'_place').html(widget);
+                if(preval){
+                    if(item.customSet){
+                        item.customSet(item,preval);
+                    }else{
+                        $('#'+item.jsonfield).val(preval);
+                    }
                 }
             }
-        }
-        if(item.afterGen){
-            item.afterGen(item);
-        }
-    },
-
-    initialize : function(item, obj_type){
-        var that= this;
-        this.item_type = obj_type;
-        this.delegateEvents(_.extend(this.events,{'submit' : 'submit'}));
-        if(item === false){
-            $('#form-title').html(that.form_title[that.item_type]);
-            this.url = window.ahr.app_urls.addmarketitem+obj_type;
-            _.each(this.widget_arrs[obj_type],function(item){
-                that.makeWidget(item);
-            });
-        }else{
-            this.getItem(item,function(item_obj){
-                that.url = window.ahr.app_urls.editmarketitem+item_obj[0].pk;
-                that.item_type = item_obj[0].fields.item_type;
-                that.setForm(item_obj, that.item_type);
-                $('#form-title').html(that.form_title[that.item_type]);
-            });
-        }
-
-    },
-
-    submit: function(e){
-        var that = this;
-        e.preventDefault();
-        var dfrd = $.ajax({
-            type: 'POST',
-            url: this.url,
-            dataType:'json',
-            traditional: true,
-            data: this.getFormData()
-        });
-        dfrd.done(function(){
-            window.location = window.ahr.app_urls.market;
-        });
-
-        dfrd.fail(function(data){
-            for(item in data.responseJSON.errors){
-                $('.'+data.responseJSON.errors[item][0]+'.error').html(data.responseJSON.errors[item][1]);
+            if(item.afterGen){
+                item.afterGen(item);
             }
-            that.alert('Correct the errors (you have to select items from drop down menu)','#itemformerror');
-        });
-        return false;
-    }
-});
+        },
+    
+        initialize : function(item, obj_type){
+            var that= this;
+            this.setWidgetArr();
+            this.item_type = obj_type;
+            this.delegateEvents(_.extend(this.events,{'submit' : 'submit'}));
+            if(item === false){
+                $('#form-title').html(that.form_title[that.item_type]);
+                this.url = window.ahr.app_urls.addmarketitem+obj_type;
+                _.each(this.widget_arrs[obj_type],function(item){
+                    that.makeWidget(item);
+                });
+            }else{
+                this.getItem(item,function(item_obj){
+                    that.url = window.ahr.app_urls.editmarketitem+item_obj[0].pk;
+                    that.item_type = item_obj[0].fields.item_type;
+                    that.setForm(item_obj, that.item_type);
+                    $('#form-title').html(that.form_title[that.item_type]);
+                });
+            }
+    
+        },
+    
+        submit: function(e){
+            var that = this;
+            e.preventDefault();
+            var dfrd = $.ajax({
+                type: 'POST',
+                url: this.url,
+                dataType:'json',
+                traditional: true,
+                data: this.getFormData()
+            });
+            dfrd.done(function(){
+                window.location = window.ahr.app_urls.market;
+            });
+    
+            dfrd.fail(function(data){
+                for(item in data.responseJSON.errors){
+                    $('.'+data.responseJSON.errors[item][0]+'.error').html(data.responseJSON.errors[item][1]);
+                }
+                that.alert('Correct the errors (you have to select items from drop down menu)','#itemformerror');
+            });
+            return false;
+        }
+    });
 
     window.item_form = window.item_form|| {};
     window.item_form.initItem= function(item,obj_type){
