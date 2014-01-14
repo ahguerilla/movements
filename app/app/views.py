5aafea7037aee1fa60eaa25776aa9d1c43ef7e13
Andrew Barrett <bazerk@gmail.com>
2014-01-14 11:18:00 +0000

    Remove liquid

--- app/app/views.py
+++ app/app/views.py
@@ -1,16 +1,14 @@
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from django.template import RequestContext, loader
+from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 
 def home(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect(reverse('exchange'))
-
     return render_to_response('home.html', {}, context_instance=RequestContext(request))
 
 
 def exchange(request):
     return HttpResponseRedirect(reverse('show_market'))
-    # return render_to_response('exchange.html', {}, context_instance=RequestContext(request))
