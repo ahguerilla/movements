b5699954353578a667ed1bbcea5a04d0775dbcf9
mani <mani@guerillasoftware.net>
2014-01-24 13:24:45 +0000

    report admin

--- app/app/market/admin/report.py
+++ app/app/market/admin/report.py
@@ -3,6 +3,12 @@ import app.market.models as models
 import django.db as db
 
 class MarketItemPostReportAdmin(admin.ModelAdmin):
-    list_display = ('item', 'contents', 'owner','resolved' )
+    list_display = ('item_owner', 'reportedby', 'pub_date', 'resolved' )
+    readonly_fields = ('owner', 'item')
+    def item_owner(self,obj):
+        return obj.item.owner.username
+
+    def reportedby(self,obj):
+        return obj.owner.username
 
 admin.site.register(models.MarketItemPostReport, MarketItemPostReportAdmin)
