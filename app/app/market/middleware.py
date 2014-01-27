ac8804074138460e6a95f09cf3c216944c829152
mani <mani@guerillasoftware.net>
2014-01-27 11:01:07 +0000

    user can set and change profile picture and password from getting-started page

--- app/app/market/middleware.py
+++ app/app/market/middleware.py
@@ -1,11 +1,20 @@
 from django.shortcuts import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
 class NoUserProfile(object):
     def process_request(self, request):
         if not request.user.is_anonymous() and not request.user.is_superuser and not hasattr(request.user,'userprofile') and (
             request.path != reverse('getting_started') and
-            request.path != reverse('account_logout')
+            request.path != reverse('account_logout') and
+            request.path != reverse('avatar_change') and
+            request.path != reverse('avatar_add') and
+            '/'.join(request.path.split('/')[:-2]) != '/'.join(reverse('avatar_render_primary',args=(request.user.username,120)).split('/')[:-2]) and
+            '/'.join(request.path.split('/')[:-1]) != '/'.join(reverse('get_avatar',args=('json',request.user.id,80)).split('/')[:-1]) and
+            request.path != reverse('avatar_add') and
+            not request.path.startswith(settings.MEDIA_URL+'avatars/'+request.user.username) and
+            request.path != reverse('account_change_password') and
+            request.path != reverse('account_change_password_success')
             ):
             return HttpResponseRedirect(reverse('getting_started'))
         return None
