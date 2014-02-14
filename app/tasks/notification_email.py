876f0c45303ae581208574c183e66dc30cfcd410
mani <mani@guerillasoftware.net>
2014-02-14 17:18:37 +0000

    copies of t&c privacy and contact

--- app/tasks/notification_email.py
+++ app/tasks/notification_email.py
@@ -33,18 +33,7 @@ def sendNotification(notifications):
     mail.content_subtype = 'html'
     mail.send()
 
-if __name__ == '__main__':
-    #afile = open('/home/mani/Desktop/skills.txt','r')
-    #items=afile.readlines()
-    #it=[]
-    #for item in items:
-        #it.append(item.strip())
-    #afile.close()
-    #string = render_to_string('emails/crap.txt',{'items':it})    
-    #bfile = open('/home/mani/Desktop/skills.fix.txt','w')
-    #bfile.writelines(string)
-    #bfile.close()
-    
+if __name__ == '__main__':   
     notifications = Notification.objects.filter(user__userprofile__get_newsletter=True,
         pub_date__gte=datetime.now()-timedelta(int(constance.config.EMAIL_NOTIFICATION_INTERVAL))).\
         order_by('user','pub_date')        
