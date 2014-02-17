e27d704991f5f886d7a44d7e39820f68480bc080
Andrew Barrett <bazerk@gmail.com>
2014-02-17 15:42:20 +0000

    Try and set path properly for beanstalk

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
 import os
+import sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.app.settings.testing")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.testing")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
