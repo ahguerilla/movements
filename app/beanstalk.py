8d9ca74a0ac9ec20ce53dd8cc330a81dd3662aa6
Andrew Barrett <bazerk@gmail.com>
2014-02-17 16:04:20 +0000

    still no settings

--- app/beanstalk.py
+++ app/beanstalk.py
@@ -1,9 +1,13 @@
 import os
 import sys
-
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.testing")
+from django.core.management import setup_environ
+from app.settings import testing
+
+setup_environ(testing)
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.testing")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
