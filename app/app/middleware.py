326b60324ca72192ee0206fa3eaf9b9813e395ed
Andrew Barrett <bazerk@gmail.com>
2014-02-18 15:29:26 +0000

    Another tweak

--- app/app/middleware.py
+++ app/app/middleware.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 
 class SSLRedirect:
     def process_request(self, request):
-        if not any([settings.DEBUG, request.is_secure(), request.META.get("X-Forwarded-Proto", "") == 'https']):
+        if not any([settings.DEBUG, request.is_secure(), request.META.get("X-Forwarded-Proto", "") == 'https', request.META.get("HTTP_X_FORWARDED_PROTO", "") == 'https']):
             url = request.build_absolute_uri(request.get_full_path())
             if url.startswith('http://'):
                 secure_url = url.replace("http://", "https://")
