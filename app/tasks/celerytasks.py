38deeed27a4fa5238633c85dd83aa66c547f1429
mani <mani@guerillasoftware.net>
2014-03-13 14:31:27 +0000

    change mail notificatoins to daily

--- app/tasks/celerytasks.py
+++ app/tasks/celerytasks.py
@@ -10,7 +10,7 @@ if not '_app' in dir():
     _app = Celery('celerytasks', broker=settings.CELERY_BROKER)
 
 
-def get_notification_text(obj, update=False):    
+def get_notification_text(obj, update=False):
     return json.dumps({
         'update':update,
         'title': obj.title
@@ -25,20 +25,20 @@ def get_notification_comment_text(obj, username, comment, not_yours=False):
         'not_yours': not_yours
     })
 
-   
+
 def find_people_interested_in(obj):
     skills = [skill.id for skill in obj.skills.all()]
     countries = [country.id for country in obj.countries.all()]
     issues = [issue.id for issue in obj.issues.all()]
     query = Q(skills__in=skills) | Q(issues__in=issues) | Q(countries__in=countries)
-    query = query & ~Q(user=obj.owner) 
+    query = query & ~Q(user=obj.owner)
     profiles = UserProfile.objects.filter(query).distinct('id').only('user').all()
     return profiles
 
 
 @_app.task(name="createNotification", bind=True)
 def create_notification(self, obj):
-    notifications = [ notif.user for notif in Notification.objects.filter(item=obj.id).only('user').all()]    
+    notifications = [ notif.user for notif in Notification.objects.filter(item=obj.id).only('user').all()]
     profiles = find_people_interested_in(obj)
     for profile in profiles:
         if profile.user.id in notifications:
@@ -53,7 +53,7 @@ def create_notification(self, obj):
 
 
 @_app.task(name="createCommentNotification", bind=True)
-def create_comment_notification(self, obj, comment, username):    
+def create_comment_notification(self, obj, comment, username):
     created = []
     if obj.owner.username != username:
         notification = Notification()
@@ -61,19 +61,19 @@ def create_comment_notification(self, obj, comment, username):
         notification.item = obj
         notification.avatar_user = username
         notification.comment_id = comment.id
-        notification.text = get_notification_comment_text(obj,username,comment)    
-        notification.save()    
+        notification.text = get_notification_comment_text(obj,username,comment)
+        notification.save()
         created.append(obj.owner.id)
-    for cmnt in obj.comments.all():        
+    for cmnt in obj.comments.all():
         if cmnt.owner.username != username and cmnt.owner.id not in created and cmnt.deleted==False:
             notification = Notification()
             notification.user = cmnt.owner
             notification.item = obj
             notification.avatar_user = username
             notification.comment_id = comment.id
-            notification.text = get_notification_comment_text(obj,username,comment,True)    
-            notification.save()            
-            created.append(cmnt.owner.id)    
+            notification.text = get_notification_comment_text(obj,username,comment,True)
+            notification.save()
+            created.append(cmnt.owner.id)
     return
 
 
@@ -82,10 +82,10 @@ def update_notifications(self, obj):
     notification_objs = Notification.objects.filter(item=obj.id).only('user','read').all()
     notification_userids =set(notification.user.id for notification in notification_objs )
     profiles = find_people_interested_in(obj)
-    user_ids = set(profile.user.id for profile in profiles)    
+    user_ids = set(profile.user.id for profile in profiles)
 
-    notifications_tocereate = user_ids.difference(notification_userids)    
-    for user_id in notifications_tocereate:        
+    notifications_tocereate = user_ids.difference(notification_userids)
+    for user_id in notifications_tocereate:
         notification = Notification()
         notification.user_id = user_id
         notification.item = obj
@@ -93,19 +93,19 @@ def update_notifications(self, obj):
         notification.text = get_notification_text(obj)
         notification.save()
 
-    notifications_toupdate = user_ids.intersection(notification_userids)    
-    for user_id in notifications_toupdate:        
+    notifications_toupdate = user_ids.intersection(notification_userids)
+    for user_id in notifications_toupdate:
         notification = Notification()
         notification.user_id = user_id
         notification.item = obj
         notification.avatar_user = obj.owner.username
         notification.text = get_notification_text(obj,update=True)
-        notification.save()                        
+        notification.save()
     return
 
 
 @_app.task(name="markReadNotifications", bind=True)
-def mark_read_notifications(self, obj_ids, user_id):    
-    notifications = Notification.objects.filter(user=user_id).filter(item__in=obj_ids).filter(read=False).update(read=True)    
+def mark_read_notifications(self, obj_ids, user_id):
+    notifications = Notification.objects.filter(user=user_id).filter(item__in=obj_ids).filter(read=False).update(read=True)
     return
 
