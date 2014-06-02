c2336e147818ec5349bd6097c6dec1df60413c94
s_kupr <semargal@gmail.com>
2014-06-02 15:48:56 +0300

    Added staff_owner to market item

--- app/app/market/admin/offer.py
+++ app/app/market/admin/offer.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 import app.market.models as models
 import django.db as db
+from django.contrib.auth.models import User
 
 
 class OfferManager(db.models.Manager):
@@ -21,6 +22,12 @@ class OfferAdmin(admin.ModelAdmin):
         if obj:
             return self.readonly_fields + ('owner', 'pub_date', 'closed_date')
         return self.readonly_fields
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        if db_field.name == 'staff_owner':
+            kwargs['queryset'] = User.objects.filter(is_staff=True)
+        return super(OfferAdmin, self).formfield_for_foreignkey(
+            db_field, request, **kwargs)
 
 
 admin.site.register(Offer, OfferAdmin)
--- app/app/market/admin/request.py
+++ app/app/market/admin/request.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 import app.market.models as models
 import django.db as db
+from django.contrib.auth.models import User
 
 
 class RequestManager(db.models.Manager):
@@ -20,6 +21,12 @@ class RequestAdmin(admin.ModelAdmin):
         if obj:
             return self.readonly_fields + ('owner', 'pub_date', 'closed_date')
         return self.readonly_fields
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        if db_field.name == 'staff_owner':
+            kwargs['queryset'] = User.objects.filter(is_staff=True)
+        return super(RequestAdmin, self).formfield_for_foreignkey(
+            db_field, request, **kwargs)
 
 
 admin.site.register(Request,RequestAdmin)
