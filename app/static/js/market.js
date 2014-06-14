02f2695dd60c59edc767c627d128013fed1e6da1
Andrew Barrett <bazerk@gmail.com>
2014-06-14 20:30:42 +0100

    Just rip out a bunch of code

--- app/static/js/market.js
+++ app/static/js/market.js
@@ -1,41 +1,6 @@
 (function () {
-  $('.nanamorde').hide();
-
-  var MarketRoute = Backbone.Router.extend({
-    routes: {
-      "": "page",
-      ":itemid": "updateViews",
-      "item/:item_id": "gotoItem"
-    },
-    firstTime: true,
-    emptyPage: true,
-
-    updateViews: function(item){
-      this.market.setViewsCount(item);
-      this.page();
-    },
-
-    gotoItem: function (item) {
-      this.market.showItem(item);
-    },
-
-    page: function () {
-      if (this.firstTime) {
-        this.market.showMarket();
-        this.market.initInfiniteScroll();
-        this.market.scrollBack();
-
-        this.firstTime = false;
-      } else {
-        this.market.resetSingle();
-        this.market.showMarket();
-        this.market.refreshScrollElements();
-        this.market.scrollBack();
-      }
-    },
-
-    initialize: function (market) {
-      this.market = market;
+  var MarketFilterView = Backbone.View.extend({
+    events: {
     }
   });
 
@@ -45,63 +10,22 @@
       "Request": "request"
     },
 
-    isShowingHidden:function(){
-      return this.filter_widget.filters.showHidden;
-    },
-
-    setViewsCount: function(id){
-      $.getJSON(
-        window.ahr.app_urls.getViewsCount + id,
-        function (data) {
-          $('.market-item-card[item_id="' + id + '"] .views-counter').text(data.result);
-      });
-    },
-
-    gotoItem: function(ev){
-      window.location = ev.currentTarget.getAttribute('href');
-    },
-
-    edit_callback: function (item_id) {
-      var that = this;
-      var dfrd = $.ajax({
-        url: this.getItem + item_id
-      });
-      dfrd.done(function (item) {
-        var html = that.item_widget.reloadItem(item);
-        html = that.get(item[0].fields);
-        that.truncateLongText(html, item.pk);
-        $('.market-place-item[item_id=' + item_id + ']').replaceWith(html);
-        that.item_widget.afterset('.market-place-item[item_id=' + item_id + ']');
-        if (that.isSingle() === false) {
-          that.fancyref(html);
-        }
-      });
-    },
-
     initialize: function (filters) {
       this.item_type = 'item';
       this.getitemfromto = window.ahr.app_urls.getmarketitemfromto;
-      this.viewurl = window.ahr.app_urls.viewitem;
       this.item_tmp = _.template($('#item_template').html());
-      this.requiresResetOnNewOfferRequest = true;
-      del_func = _.bind(this.del_callback, this);
-      edit_func = _.bind(this.edit_callback, this);
-      this.item_widget = window.ahr.marketitem_widget.initWidget('body', this, del_func, edit_func);
-      this.getItem = window.ahr.app_urls.getmarketitem;
       this.init(filters);
-      this.filter_widget.filters.types = ["offer", "request"];
-      this.filter_widget.types = this.types;
       _.extend(this.events,{'click .routehref': 'gotoItem'} );
       return this;
-    },
+    }
   });
 
   window.ahr = window.ahr || {};
   window.ahr.market = window.ahr.market || {};
   window.ahr.market.initMarket = function (filters) {
-    var market = new MarketView(filters);
-    new MarketRoute(market);
-    Backbone.history.start();
+    var filterView = new MarketFilterView({el: ''});
+    var market = new MarketView(filterView);
+    market.initInfiniteScroll();
     document.title = window.ahr.string_constants.exchange;
   };
 })();
