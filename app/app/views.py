b05b452bcc6955778405fe723fb98d2950c286c6
Aidan Hamade <aidan.hamade@gmail.com>
2014-06-18 16:50:39 +0100

    remove a bunch of unused templates

--- app/app/views.py
+++ app/app/views.py
@@ -2,9 +2,7 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from django.contrib.auth.decorators import login_required
 from django.conf import settings as django_settings
-
 
 def home(request):
     if request.user.is_authenticated() and not request.user.is_superuser:
@@ -25,16 +23,6 @@ def terms_and_conditions(request):
 def contact_us(request):
     return render_to_response('ahr/contact_us.html', context_instance=RequestContext(request))
 
-
-def privacy(request):
-    return render_to_response('ahr/privacy.html', context_instance=RequestContext(request))
-
-def how_it_works_pub(request):
-    return render_to_response('ahr/how_it_works_public.html', context_instance=RequestContext(request))
-
-@login_required
-def how_it_works_priv(request):
-    return render_to_response('ahr/how_it_works_private.html', context_instance=RequestContext(request))
 
 def exchange(request):
     return HttpResponseRedirect(reverse('show_market'))
