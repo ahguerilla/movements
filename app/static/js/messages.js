bce1efa07915bddc00f4cd96705277199e3671c6
Andrew Barrett <bazerk@gmail.com>
2014-06-23 08:23:34 +0100

    Clean up messages a bit

--- app/static/js/messages.js
+++ app/static/js/messages.js
@@ -3,48 +3,26 @@
     el: '#postman',
     events: {
       'click .conv_link': 'openConv',
-      'click #backtofolder': 'back',
-      'click .next': 'addNext'
+      'click #backtofolder': 'back'
     },
 
-    addNext: function (ev) {
-      ev.preventDefault();
-      var dfrd = $.ajax({
-        url: ev.currentTarget.href
-      });
-      dfrd.done(function (data) {
-        if ($('.next', $(data)).hasClass('disabled')) {
-          $('#paginationblock').remove();
-        }
-//        var more = $('.next', $(data))[0];
-//        $('.next').replaceWith(more);
-//        $(more).html('<button style="margin-top:5px;" class="btn btn-default">' + window.ahr.string_constants.more + '</button>');
-        $('.messagelist').append($('.messagelist', $(data)).children());
-      });
-    },
-
-    reply: function (ev) {
-      ev.reventDefault();
-      return false;
-    },
-
-    postProccessConv: function(data){
+    postProcessConv: function(data){
       var that = this;
-      data1 = data.replace(that.itemre, function (match, item_id, offset, string) {
+      var data1 = data.replace(that.itemre, function (match, item_id, offset, string) {
         return "<a href='/market/#item/" + item_id + "'>" + window.ahr.string_constants.view_recommendation + "</a>";
       });
-      data2 = data1.replace(that.userre, function (match, username, offset, string) {
+      var data2 = data1.replace(that.userre, function (match, username, offset, string) {
         return "<a href='" + window.ahr.app_urls.viewuserprofile + username + "'>" + window.ahr.string_constants.view_recommendation + "</a>";
       });
 
-      data3 = data2.replace(that.itemre, function (match, item_id, offset, string) {
+      var data3 = data2.replace(that.itemre, function (match, item_id, offset, string) {
         return "<a href='/market/#item/" + item_id + "'>" + window.ahr.string_constants.view_recommendation + "</a>";
       });
-      data4 = data3.replace(that.userre, function (match, username, offset, string) {
+      var data4 = data3.replace(that.userre, function (match, username, offset, string) {
         return "<a href='" + window.ahr.app_urls.viewuserprofile + username + "'>" + window.ahr.string_constants.view_recommendation + "</a>";
       });
 
-      var user;
+      var user = null;
       $('.messageavatar img', data4).each(function (item, index) {
         user = $(this).attr('alt');
         if (user != window.ahr.username) return false;
@@ -88,8 +66,7 @@
       });
 
       dfrd.done(function (data) {
-        var conv = that.postProccessConv(data);
-
+        var conv = that.postProcessConv(data);
         $('#conversation').html(conv.html);
         if ($('#id_body').length > 0) {
           $('#id_body').empty();
@@ -121,9 +98,6 @@
       this.actions_view = window.ahr.actions_view();
       this.itemre = new RegExp(/&lt;!--item=&quot;(\d+)&quot;--&gt;/);
       this.userre = new RegExp(/&lt;!--user=&quot;(\S+)&quot;--&gt;/);
-//      var more = $('.next')[0];
-//      $(more).html('<button style="margin-top:5px;" class="btn btn-default">' + window.ahr.string_constants.more + '</button>');
-//      $('#paginationblock').html(more);
       $('#conversation-cont').hide();
     }
   });
@@ -131,7 +105,7 @@
   window.ahr = window.ahr || {};
   window.ahr.messages = window.ahr.messages || {};
   window.ahr.messages.initInbox = function () {
-    var messages = new InboxView();
+    new InboxView();
     $('#back').hide();
   };
 })();
