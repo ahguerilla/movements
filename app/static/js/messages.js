58e93b7e597adf3a2d850d8464e09173bb6ce24f
Andrew Barrett <bazerk@gmail.com>
2014-06-23 14:13:27 +0100

    Reasonably happy with the styling of messages, needs a bit more ux work

--- app/static/js/messages.js
+++ app/static/js/messages.js
@@ -1,9 +1,18 @@
 (function () {
   var InboxView = Backbone.View.extend({
     el: '#postman',
+    folderType: 'inbox',
+    leftColBaseHeight: 0,
     events: {
-      'click .conv_link': 'openConv',
-      'click #backtofolder': 'back'
+      'click #reply-btn': 'replyToConversation',
+      'click .sidebar .message': 'openConversationFromClick',
+      'click #conversation .message-header a': 'stopBubble',
+      'click #conversation .message-header': 'toggleMessage',
+      'rezie #conversation': 'resizeSideBar'
+    },
+
+    stopBubble: function(ev) {
+      ev.stopPropagation();
     },
 
     postProcessConv: function(data){
@@ -23,10 +32,6 @@
       });
 
       var user = null;
-      $('.messageavatar img', data4).each(function (item, index) {
-        user = $(this).attr('alt');
-        if (user != window.ahr.username) return false;
-      });
       if (user == window.ahr.username) {
         user = $('.pm_recipient', data4).text();
         if (user =="<me>") user = window.ahr.username;
@@ -34,68 +39,70 @@
       return ({'user':user, 'html':data4});
     },
 
-    setMessageCounter: function(){
-      $.getJSON(window.ahr.app_urls.getmessagecount, function (data) {
-        $('.message-counter').each(function (tmp, item) {
-          if (data > 0) {
-            $('#msgcntr', $(item)).text('(' + data + ')');
-          } else {
-            $('#msgcntr', $(item)).text('');
-          }
-        });
-      });
+    openConversationFromClick: function(ev) {
+      ev.preventDefault();
+      this.openConversation($(ev.currentTarget));
     },
 
-    openConv: function (ev) {
-      ev.preventDefault();
-      $('#conversation').hide();
-      var that = this;
-      var subject = $('.subject', $(ev.currentTarget)).children();
-      if (subject.is('strong')) {
-        subject.replaceWith(subject.text());
+    openConversation: function ($message) {
+      $('#conversation').html('');
+
+      this.$el.find('.sidebar .message').removeClass('selected');
+      $message.addClass('selected');
+      if (this.folderType == 'inbox') {
+        if ($message.hasClass('new')) $message.removeClass('new');
       }
 
-      if (ev.currentTarget.parentElement.tagName == "STRONG") {
-        a = ev.currentTarget;
-        $(ev.currentTarget.parentElement).html(a);
-      }
-
-      var dfrd = $.ajax({
-        url: ev.currentTarget.getAttribute('href'),
-        dataType: 'html'
-      });
-
-      dfrd.done(function (data) {
-        var conv = that.postProcessConv(data);
-        $('#conversation').html(conv.html);
-        if ($('#id_body').length > 0) {
-          $('#id_body').empty();
-          window.ahr.expandTextarea('#id_body');
+      $.ajax({
+        url: $message.attr('href'),
+        dataType: 'html',
+        context: this,
+        success: function(data) {
+          var conv = this.postProcessConv(data);
+          $('#conversation').html(conv.html);
+          if ($('#id_body').length > 0) {
+            $('#id_body').empty();
+            window.ahr.expandTextarea('#id_body');
+          }
+          this.showconv();
         }
-        that.showconv();
-        that.setMessageCounter();
       });
+
       return false;
     },
 
     showconv: function () {
       $('#conversation').show();
-      $('#conversation-cont').show();
-      $('#back').show();
-      $('#breadsubject').text($('#messagesubjectheader').text());
       $('body').scrollTop(0);
     },
 
-    back: function (ev) {
-      $('#conversation-cont').hide();
-      $('#back').hide();
+    replyToConversation: function(ev) {
+      ev.preventDefault();
+      if ($('#id_body').val() != "") {
+        $('#replyform').submit();
+      } else {
+        alert('{%trans "You cant send an empty message."%}');
+      }
+      return false;
+    },
+
+    toggleMessage: function(ev) {
+      var $messageBody = $(ev.currentTarget).parents('.message').find('.message-body');
+      $messageBody.toggleClass('collapse');
     },
 
     initialize: function () {
-      this.actions_view = window.ahr.actions_view();
       this.itemre = new RegExp(/&lt;!--item=&quot;(\d+)&quot;--&gt;/);
       this.userre = new RegExp(/&lt;!--user=&quot;(\S+)&quot;--&gt;/);
-      $('#conversation-cont').hide();
+      this.folderType = this.$el.data('folder-type');
+      $('#conversation').html('');
+
+      this.leftColBaseHeight = this.$el.find('.sidebar').height();
+
+      var $messages = this.$el.find('.sidebar .message:first-child');
+      this.openConversation($messages);
+
+      this.$el.find('#conversation').css('min-height', this.$el.find('.sidebar').height());
     }
   });
 
@@ -103,6 +110,5 @@
   window.ahr.messages = window.ahr.messages || {};
   window.ahr.messages.initInbox = function () {
     new InboxView();
-    $('#back').hide();
   };
 })();
