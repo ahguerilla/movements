2e5040ab6c3c1ecf68e58bd649239b1d19559e55
Aidan Hamade <aidan.hamade@gmail.com>
2014-07-04 14:38:48 +0100

    implement new navigation

--- app/app/settings/cms_settings.py
+++ app/app/settings/cms_settings.py
@@ -9,6 +9,7 @@ CMS_PLACEHOLDER_CACHE = False
 
 CMS_TEMPLATES = (
     ('cms/single_page.html', 'Single Page'),
+    ('cms/page_with_left_nav.html', 'Page With Left Nav'),
     ('cms/empty.html', 'Empty page'),
 )
 
