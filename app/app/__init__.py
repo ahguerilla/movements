1b920d70485cebd22b4bbd8be771bf17af575b93
Andrew Barrett <bazerk@gmail.com>
2015-02-09 12:25:00 +1300

    Monkey patch admin view to ratelimit admin logins

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,5 +1,16 @@
 from __future__ import absolute_import
 
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
-#from tasks.celerytasks import _app as celery_app
+from django.contrib import admin
+from django.http import HttpResponseForbidden
+from ratelimit.decorators import ratelimit
+
+old_login = admin.site.login
+
+
+@ratelimit(key='ip', rate='5/m', method=['POST'])
+def login_ratelimited(request, **kwargs):
+    if request.limited:
+        return HttpResponseForbidden('<h1>Rate limit exceeded</h1>')
+    return old_login(request, **kwargs)
+
+admin.site.login = login_ratelimited
--- app/app/urls.py
+++ app/app/urls.py
@@ -3,10 +3,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 
-from adminplus.sites import AdminSitePlus
 from django.contrib import admin
 
-admin.site = AdminSitePlus()
+
 admin.autodiscover()
 
 js_info_dict = {
