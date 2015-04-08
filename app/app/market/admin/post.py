66930ad6482bda28647734c50b05cbbf26f756c4
Andrew Barrett <bazerk@gmail.com>
2015-04-08 14:27:47 +1200

    Invalidate translation cache when editing a post in the admin

---
+++ app/app/market/admin/post.py
@@ -0,0 +1,52 @@
+from django.contrib import admin
+from django.contrib.auth.models import User
+import django.db as db
+
+
+import app.market.models as models
+from app.market.tasks.translations import mark_translations_for_update
+
+
+class PostManager(db.models.Manager):
+    def __init__(self, request_type):
+        super(PostManager, self).__init__()
+        self.request_type = request_type
+
+    def get_query_set(self):
+        return super(PostManager, self).get_query_set().filter(item_type=self.request_type)
+
+
+class Request(models.MarketItem):
+    class Meta:
+        proxy = True
+    objects = PostManager('request')
+
+
+class Offer(models.MarketItem):
+    class Meta:
+        proxy = True
+    objects = PostManager('offer')
+
+
+class PostAdmin(admin.ModelAdmin):
+    exclude = ('item_type',)
+    list_display = ('title', 'owner', 'pub_date', 'published')
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ('owner', 'closed_date')
+        return self.readonly_fields
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        if db_field.name == 'staff_owner':
+            kwargs['queryset'] = User.objects.filter(is_staff=True)
+        return super(PostAdmin, self).formfield_for_foreignkey(
+            db_field, request, **kwargs)
+
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        mark_translations_for_update(obj)
+
+
+admin.site.register(Offer, PostAdmin)
+admin.site.register(Request, PostAdmin)
