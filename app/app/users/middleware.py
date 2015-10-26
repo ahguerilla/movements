1d45415b67507d5323f8c08778fc941cf176ce2d
Aidan Hamade <aidan.hamade@gmail.com>
2015-10-26 12:52:04 +0000

    fix admin enabled on live

--- app/app/users/middleware.py
+++ app/app/users/middleware.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from two_factor.utils import default_device
+from django.conf import settings
 
 
 class FirstLoginMiddleware(object):
@@ -22,21 +23,20 @@ class FirstLoginMiddleware(object):
 
 class ForceTwoFactorForStaffMiddleware(object):
     """ Force admin to have 2 factor enabled """
-
     def process_request(self, request):
-        allowed_urls = [reverse('admin:logout')]
-        requested_url = request.get_full_path()
-        two_factor_base = reverse('two_factor:profile')
-        if two_factor_base not in requested_url \
-           and requested_url not in allowed_urls \
-           and request.user and any([request.user.is_superuser, request.user.is_staff]) \
-           and not default_device(request.user):
-                return HttpResponseRedirect(reverse('two_factor:setup'))
+        if settings.ADMIN_ENABLED:
+            allowed_urls = [reverse('admin:logout')]
+            requested_url = request.get_full_path()
+            two_factor_base = reverse('two_factor:profile')
+            if two_factor_base not in requested_url \
+               and requested_url not in allowed_urls \
+               and request.user and any([request.user.is_superuser, request.user.is_staff]) \
+               and not default_device(request.user):
+                    return HttpResponseRedirect(reverse('two_factor:setup'))
 
 
 class CsrfLoginMiddleware(object):
     """ Set login csrftoken to httponly. """
-
     def process_response(self, request, response):
         current_url = request.get_full_path()
         target_url = reverse('account_login')
