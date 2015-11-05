34ffc6df7bc8d84d0abdd102bef789ddba7877ae
Aidan Hamade <aidan.hamade@gmail.com>
2015-11-05 09:40:57 +0000

    add translations for home page banner

--- app/app/translation.py
+++ app/app/translation.py
@@ -2,24 +2,35 @@ from modeltranslation.translator import translator, TranslationOptions
 from app.users.models import (
     Countries, Skills, Issues, Nationality, Residence,
     Language, Region, Interest)
+from app.models import HomePageBanner
+
 
 class ResidenceTranslationOptions(TranslationOptions):
     fields = ('residence',)
 
+
 class CountriesTranslationOptions(TranslationOptions):
     fields = ('countries',)
+
 
 class NationalityTranslationOptions(TranslationOptions):
     fields = ('nationality',)
 
+
 class SkillsTranslationOptions(TranslationOptions):
     fields = ('skills',)
+
 
 class IssuesTranslationOptions(TranslationOptions):
     fields = ('issues',)
 
+
 class PlaceholderTranslationOptions(TranslationOptions):
     fields = ('content',)
+
+
+class HomePageBannerOptions(TranslationOptions):
+    fields = ('title_text', 'sub_text',)
 
 
 class NamedObjectTranslationOptions(TranslationOptions):
@@ -31,6 +42,7 @@ translator.register(Countries, CountriesTranslationOptions)
 translator.register(Nationality, NationalityTranslationOptions)
 translator.register(Skills, SkillsTranslationOptions)
 translator.register(Issues, IssuesTranslationOptions)
+translator.register(HomePageBanner, HomePageBannerOptions)
 
 translator.register(Language, NamedObjectTranslationOptions)
 translator.register(Region, NamedObjectTranslationOptions)
