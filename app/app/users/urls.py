016b8de6176f2a393f7d2b49669b387eeff3a29a
Aidan Hamade <aidan.hamade@gmail.com>
2015-12-17 14:38:21 +0000

    remove unused url

--- app/app/users/urls.py
+++ app/app/users/urls.py
@@ -6,7 +6,6 @@ urlpatterns = patterns("",
     url(r'^settings$', views.settings, name='user_settings'),
     url(r'^profile$', views.profile, name='user_profile'),
     url(r'^profile/(?P<user_name>\S+)$', views.profile_for_user, name='user_profile_for_user'),
-    url(r'^waitforactivation$', views.waitforactivation, name="waitforactivation"),
     url(r'^thanksforactivation$', views.thanksforactivation, name="thanksforactivation"),
     url(r"^confirm_email/(?P<key>\w+)/$", views.confirm_email, name="users_confirm_email"),
 )
