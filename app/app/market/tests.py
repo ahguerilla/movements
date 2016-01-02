9eb8132ce0709739a2ed1ab495e785f6a7aa0bf0
Aidan Hamade <aidan.hamade@gmail.com>
2016-01-02 13:27:34 +0000

    add unit test infrastructure

--- app/app/market/tests.py
+++ app/app/market/tests.py
@@ -1,43 +1,11 @@
+# coding=utf-8
+
 from django.test import TestCase
-from django.conf import settings
-from django.test import LiveServerTestCase
-from selenium.webdriver.firefox.webdriver import WebDriver
-import selenium.webdriver.support.ui as ui
-import time
+import opengraph
 
 
-class MarketSeleniumTests(LiveServerTestCase):
-    fixtures = ['test_data.json']
-
-    @classmethod
-    def setUpClass(cls):
-        cls.selenium = WebDriver()
-        cls.wait = ui.WebDriverWait(cls.selenium,1000)
-        super(MarketSeleniumTests, cls).setUpClass()
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.selenium.quit()
-        super(MarketSeleniumTests, cls).tearDownClass()
-
-
-    def login(self):
-        self.selenium.get('%s%s' % (self.live_server_url, '/accounts/login/'))
-        username_input = self.selenium.find_element_by_id("id_login")
-        username_input.send_keys('manis')
-        password_input = self.selenium.find_element_by_id("id_password")
-        password_input.send_keys('123456')
-        password_input.submit()
-        selenium = self.selenium
-        self.wait.until(lambda selenium: selenium.find_element_by_id("q"))
-
-
-    def test_market_search(self):
-        self.login()
-        time.sleep(2)
-        search = self.selenium.find_element_by_id("q")
-        search.send_keys('best')
-        self.selenium.find_element_by_id("searchbtn").click()
-        selenium = self.selenium
-        self.wait.until(lambda selenium: selenium.find_element_by_class_name('market-place-item'))
-
+class OpenGraphTestCase(TestCase):
+    def test_open_graph(self):
+        test_graph_data = opengraph.OpenGraph(url="/market/tests/opengraph.html")
+        self.assertTrue(test_graph_data.is_valid())
+        self.assertEqual(test_graph_data.title, u'Graph Test Title')
---
+++ app/app/settings/test.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import
+
+from .base import *
+
+########## TEST SETTINGS
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+TEST_DISCOVER_TOP_LEVEL = SITE_ROOT
+TEST_DISCOVER_ROOT = SITE_ROOT
+ASSETS_ROOT = SITE_ROOT + '/static'
+TEST_DISCOVER_PATTERN = "test_*.py"
+SECRET_KEY = 've4&7oc3yk+g-_1ob4n_$vwn_os!=t#zn38p)fw6^q9h1a()-b'
+ASSETS_DEBUG = False
+COOKIE_DOMAIN = None
+########## IN-MEMORY TEST DATABASE
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
+}
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+CONSTANCE_DATABASE_CACHE_BACKEND = 'default'
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+        'URL': 'http://127.0.0.1:8080/solr'
+    }
+}
+
+SOUTH_TESTS_MIGRATE = False
